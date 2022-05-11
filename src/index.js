f8ea422cddfd2eb5bd240060701b7cb00f434781
luduku12 <mayranl@3il.fr>
2022-05-11 15:46:17 +0200

    vuejs

--- src/index.js
+++ src/index.js
@@ -2,26 +2,42 @@
 
 const express = require('express');
 var mysql = require('mysql');
-const path = require('path')
+const path = require('path');
 const db = require("./db");
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')))
-
-
+//app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
+//app.use('/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')))
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
 
-   res.sendFile(path.join(__dirname, 'views/index.html'));
-   const createtableretour = db.createTable(); 
-    const ajoutfilm = db.insertFilm("toto"); 
-   console.log(createtableretour);
+   res.sendFile(path.join(__dirname, 'views/accueil.html'));
+   //const createtableretour = db.createTable(); 
+   //const ajoutfilm = db.insertFilm("toto"); 
+   //console.log(createtableretour);
 });
+
+app.get('/film/:id', (req, res) => {
+   res.sendFile(path.join(__dirname, 'views/test.html'));
+});
+
+app.get('/connexion', (req, res) => {
+console.log("t");
+	var user = req.body.inputPseudo;
+	var pwd = req.body.inputPseudo;
+	console.log(user);
+	console.log(pwd);
+	
+	res.sendFile(path.join(__dirname+'/accueil.html'));
+
+})
 
 app.listen(PORT, function () {
     console.log(
